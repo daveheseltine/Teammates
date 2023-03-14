52aa721bb0030e95b68d144d67adcff73d101c41
Dave Heseltine <daveomcdave@gmail.com>
2023-03-14 19:09:08 +0000

    Added javascript files for constructors

---
+++ assets/js/Manager.js
@@ -0,0 +1,21 @@
+// Require:
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+
+// Exports:
+module.exports = Manager;
